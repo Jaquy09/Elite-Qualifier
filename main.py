49140b6523cab182891d17bda82c5b81cc180b89
JSL29 <>
2021-09-13 22:25:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#WEB SCRAPING
+#Chooes a sub reddit
+#Goal: Print out usernames and their comments 
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+url=("https://www.reddit.com/r/AskReddit/comments/ogzxt1/whats_the_best_burger_chain_in_the_us/")
+
+response=requests.get(url)
+soup=BeautifulSoup(response.content, "lxml")
+
+for item in soup.select('.Post'):
+  try:
+    print(item.select('._2mHuuvyV9doV3zwbZPtIPG')[0].get_text())
+    print(item.select('_292iotee39Lmt0MkQZ2hPV RichTextJSON-root')[0].get_text())
+  except Exception as e:
+    raise e
+    print('')
